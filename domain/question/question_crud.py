77ba85ddaabe28989a7f6fafa7af6f0b0091855d
yejin <qkrdpwls2002@naver.com>
2025-03-05 16:09:55 +0900

    ✨ Feat : 회원가입 API 구현현

--- domain/question/question_crud.py
+++ domain/question/question_crud.py
@@ -10,7 +10,7 @@ def get_question_list(db: Session, skip: int = 0, limit: int = 10):
 
     total = _question_list.count()
     question_list = _question_list.offset(skip).limit(limit).all()
-    return question_list
+    return total, question_list
 
 def get_question_detail(db: Session, question_id: int):
     question = db.query(Question).get(question_id)
--- migrations/env.py
+++ migrations/env.py
@@ -5,7 +5,7 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from myapi import models
+import models
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
