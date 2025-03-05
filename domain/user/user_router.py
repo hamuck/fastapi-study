137133b7c44882fc1654564d50523c5c5fb3eb2c
yejin <qkrdpwls2002@naver.com>
2025-03-05 16:33:15 +0900

    ✨ Feat : login API 구현현

--- domain/user/user_router.py
+++ domain/user/user_router.py
@@ -1,10 +1,19 @@
-from fastapi import APIRouter
+from datetime import datetime, timedelta
+
+from fastapi import APIRouter, HTTPException
 from fastapi import Depends
+from fastapi.security import OAuth2PasswordRequestForm
+from jose import jwt
 from sqlalchemy.orm import Session
 from starlette import status
 
 from database import get_db
 from domain.user import user_crud, user_schema
+from domain.user.user_crud import pwd_context
+
+ACCESS_TOEKN_EXPIRE_MINUTES = 60 * 24
+SECRET_KEY = "29870a908b17e8ca80235b5ca097344df4e313fc4df40f1c25696a67c10a749e"
+ALGOTITHM = "HS256"
 
 router = APIRouter(
     prefix="/api/user"
@@ -12,4 +21,29 @@ router = APIRouter(
 
 @router.post("/create", status_code=status.HTTP_204_NO_CONTENT)
 def user_create(_user_create: user_schema.UserCreate, db: Session = Depends(get_db)):
+    user = user_crud.get_existing_user(db, user_create=_user_create)
+    if user:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="이미 존재하는 사용자입니다.")
     user_crud.create_user(db=db, user_create=_user_create)
+
+@router.post("/login", response_model=user_schema.Token)
+def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = user_crud.get_user(db, form_data.username)
+    if not user or not pwd_context.verify(form_data.password, user.password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticcate": "Bearer"}
+        )
+    
+    data = {
+        "sub": user.username,
+        "exp": datetime.utcnow() + timedelta(minutes=ACCESS_TOEKN_EXPIRE_MINUTES)
+    }
+    access_token = jwt.encode(data, SECRET_KEY, algorithm=ALGOTITHM)
+
+    return {
+        "access_token": access_token,
+        "token_type": "bearer",
+        "username": user.username
+    }
